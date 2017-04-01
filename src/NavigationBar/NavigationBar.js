08dff485f0cb4cbaafe923d53a10577c333044d4
Roman Quelen <rquelen@octo.com>
2017-04-01 13:15:14 +0200

    fix style navigation bar

--- src/NavigationBar/NavigationBar.js
+++ src/NavigationBar/NavigationBar.js
@@ -12,28 +12,22 @@ import {
 const NavigationBar = () => (
   <div className={navigationBar}>
     <div className={navigationBarInnerContainer}>
-      <div className={navigationBarInner}>
-        <div className={navigationBarLinks}>
-          <ul className={navigationBarUl}>
-            <li className={navigationBarLi}>
-              <NavigationLink icon="home" url="/" label="Accueil" isActive="active" />
-            </li>
-            <li className={navigationBarLi}>
-              <NavigationLink icon="info-circle" url="/about" label="À propos" isActive="" />
-            </li>
-            <li className={navigationBarLi}>
-              <NavigationLink icon="rocket" url="/login" label="Login" isActive="" />
-            </li>
-          </ul>
-        </div>
+      <div className={navigationBarLinks}>
+        <ul className={navigationBarUl}>
+          <li className={navigationBarLi}>
+            <NavigationLink icon="home" url="/" label="Accueil" isActive="active"/>
+          </li>
+          <li className={navigationBarLi}>
+            <NavigationLink icon="info-circle" url="/about" label="À propos" isActive=""/>
+          </li>
+        </ul>
+      </div>
 
-        <div>
-          <NavigationLink icon="power-off" url="/logout" label="Déconnecter" isActive="" />
-        </div>
+      <div>
+        <NavigationLink icon="power-off" url="/logout" label="Déconnecter" isActive=""/>
       </div>
     </div>
   </div>
 );
-
 
 export default NavigationBar;
